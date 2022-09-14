c19d20e5718fe4bafbba42e840650066f60b817e
Prachi Bala <prachibala330@gmail.com>
2022-09-15 01:11:50 +0600

    bug problem solve #5

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
     const currentTime = new Date().getTime();
-    const timeSpent = (currentTime - startTime) / 1000;
+    const timeSpent = parseInt((currentTime - startTime) / 1000);
 
     document.getElementById("show-time").innerHTML = `${
         startTime ? timeSpent : 0
