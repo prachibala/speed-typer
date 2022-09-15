bcec2f842ead96a03a9100ff341c86b5b186c739
Prachi Bala <prachibala330@gmail.com>
2022-09-15 20:55:47 +0600

    bug problem solve #7

--- history.js
+++ history.js
@@ -1,10 +1,10 @@
 const histories = document.getElementById("histories");
 
 function addHistory(questionText, timeTaken, errorCount) {
-  const newRow = document.createElement("div");
-  newRow.classList.add("card");
+    const newRow = document.createElement("div");
+    newRow.classList.add("card");
 
-  newRow.innerHTML = `
+    newRow.innerHTML = `
   <h3>${questionText}</h3>
   <div>
   <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
@@ -12,29 +12,34 @@ function addHistory(questionText, timeTaken, errorCount) {
   </div>
   `;
 
-  histories.appendChild(newRow);
+    histories.appendChild(newRow);
 
-  let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
-  previousTests.push({ questionText, timeTaken, errorCount });
-  localStorage.setItem("testHistory", JSON.stringify(previousTests));
+    let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+    previousTests.push({ questionText, timeTaken, errorCount });
+    localStorage.setItem("testHistory", JSON.stringify(previousTests));
 
-  displayHistory();
+    displayHistory();
 }
 
 function displayHistory() {
-  histories.innerHTML = "";
-  const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+    histories.innerHTML = "";
+    const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
 
-  previousTests.forEach((test) => {
-    const newRow = document.createElement("div");
-    newRow.classList.add("card");
+    previousTests.forEach((test) => {
+        const newRow = document.createElement("div");
+        newRow.classList.add("col");
 
-    newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+        newRow.innerHTML = `
+
+	<div class="card ">
+			<div class="card-body p-0 text-center">
+				<h5 class="card-title mb-4 ">${test.questionText}</h5>
+				<p class="card-text mb-2" >You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+				<p class="card-text" >You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+			</div>
+		</div>
   `;
 
-    histories.appendChild(newRow);
-  });
+        histories.appendChild(newRow);
+    });
 }
